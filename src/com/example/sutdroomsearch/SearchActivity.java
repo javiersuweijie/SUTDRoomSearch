a9c284ea5bf9d465acdfaf203f46b4747c3116bd
Liza Ng <liza@lnys.per.sg>
2013-12-14 16:08:22 +0800

    Remove menu stuff from SearchActivity

--- src/com/example/sutdroomsearch/SearchActivity.java
+++ src/com/example/sutdroomsearch/SearchActivity.java
@@ -2,7 +2,6 @@ package com.example.sutdroomsearch;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class SearchActivity extends Activity {
 
@@ -10,13 +9,7 @@ public class SearchActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.search, menu);
-		return true;
 	}
 
 }
