46e1a5afbff1a89bb47a689be69f531722e5f20d
Liza Ng <liza@lnys.per.sg>
2013-12-14 11:50:02 +0800

    Add elements to MapActivity layout and define onClick handlers

--- src/com/example/sutdroomsearch/MapActivity.java
+++ src/com/example/sutdroomsearch/MapActivity.java
@@ -8,92 +8,77 @@ import android.graphics.Matrix;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MapActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		LinearLayout ll = (LinearLayout) findViewById(R.id.details);
-		ll.setVisibility(View.GONE);
+
+		// Hide info pane by default
+		LinearLayout info_pane = (LinearLayout) findViewById(R.id.info_pane);
+		info_pane.setVisibility(View.GONE);
 		
-		// Generate bitmap		
+		// Display level 2 map by default
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inSampleSize = 2;
 		Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_2nd, options);
-
-		// Set touch image view
 		TouchImageView map = (TouchImageView) findViewById(R.id.map);
 		map.setImageBitmap(img);
 		map.setMaxZoom(4f);
-		map.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				TouchImageView mapView = (TouchImageView) v;
-				Log.d("Info","Clicked image on: " + mapView.last.x + ", " + mapView.last.y);
-				Matrix m = new Matrix();
-				mapView.matrix.invert(m);
-				float[] pts = {mapView.last.x, mapView.last.y};
-				m.mapPoints(pts);
-				Log.d("Info", "Mapped to real image: " + pts[0] + ", " + pts[1]);
-				TextView tv = (TextView) findViewById(R.id.coords);
-				tv.setText("Mapped to real image: " + pts[0] + ", " + pts[1]);
-				LinearLayout ll = (LinearLayout) findViewById(R.id.details);
-				ll.setVisibility(View.VISIBLE);
-			}
-		});
+	}
 
-		Button level2_east = (Button) findViewById(R.id.level2_east);
-		Button level3_east = (Button) findViewById(R.id.level3_east);
-		Button level4_east = (Button) findViewById(R.id.level4_east);
+	public void showL2Map(View view) {
+		TouchImageView map = (TouchImageView) findViewById(R.id.map);
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 2;
+		Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_2nd, options);
+		map.setImageBitmap(img);
+	}
 
-		level2_east.setOnClickListener(new View.OnClickListener() {	
-			@Override
-			public void onClick(View v) {
-				TouchImageView map = (TouchImageView) findViewById(R.id.map);
-				BitmapFactory.Options options = new BitmapFactory.Options();
-				options.inSampleSize = 2;
-				Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_2nd, options);
-				map.setImageBitmap(img);
-				Toast.makeText(getApplicationContext(), "Click on button 1", Toast.LENGTH_LONG).show();
-			}
-		});
+	public void showL3Map(View view) {
+		TouchImageView map = (TouchImageView) findViewById(R.id.map);
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 2;
+		Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_3rd, options);
+		map.setImageBitmap(img);
+	}
+
+	public void showL4Map(View view) {
+		TouchImageView map = (TouchImageView) findViewById(R.id.map);
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 2;
+		Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_4th, options);
+		map.setImageBitmap(img);
+	}
+
+	public void showRoomInfo(View view) {
+		TouchImageView mapView = (TouchImageView) view;
+		Log.d("Info","Clicked image on: " + mapView.last.x + ", " + mapView.last.y);
+		Matrix m = new Matrix();
+		mapView.matrix.invert(m);
+		float[] pts = {mapView.last.x, mapView.last.y};
+		m.mapPoints(pts);
+		Log.d("Info", "Mapped to real image: " + pts[0] + ", " + pts[1]);
+
+		// Set name
+		TextView info_name = (TextView) findViewById(R.id.info_name);
+		info_name.setText("Mapped to real image: " + pts[0] + ", " + pts[1]);
 		
-		level3_east.setOnClickListener(new View.OnClickListener() {	
-			@Override
-			public void onClick(View v) {
-				TouchImageView map = (TouchImageView) findViewById(R.id.map);
-				BitmapFactory.Options options = new BitmapFactory.Options();
-				options.inSampleSize = 2;
-				Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_3rd, options);
-				map.setImageBitmap(img);
-				Toast.makeText(getApplicationContext(), "Click on button 2", Toast.LENGTH_LONG).show();
-			}
-		});
-		
-		level4_east.setOnClickListener(new View.OnClickListener() {	
-			@Override
-			public void onClick(View v) {
-				TouchImageView map = (TouchImageView) findViewById(R.id.map);
-				BitmapFactory.Options options = new BitmapFactory.Options();
-				options.inSampleSize = 2;
-				Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.east_4th, options);
-				map.setImageBitmap(img);
-				Toast.makeText(getApplicationContext(), "Click on button 3", Toast.LENGTH_LONG).show();
-			}
-		});
+		// Show info pane
+		LinearLayout info_pane = (LinearLayout) findViewById(R.id.info_pane);
+		info_pane.setVisibility(View.VISIBLE);
+	}
 
-		EditText search = (EditText) findViewById(R.id.search_input);
-		search.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(MapActivity.this, SearchActivity.class);
-				startActivity(i);
-			}
-		});
+	public void startSearch(View view) {
+		Intent i = new Intent(MapActivity.this, SearchActivity.class);
+		startActivity(i);
+	}
+
+	public void clearSearch(View view) {
+		// Hide info pane
+		LinearLayout info_pane = (LinearLayout) findViewById(R.id.info_pane);
+		info_pane.setVisibility(View.GONE);
 	}
 }
